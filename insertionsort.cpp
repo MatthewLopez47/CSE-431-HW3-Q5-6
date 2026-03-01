44b94240a9dc598e66225a22d1fa0366d16c9c87
Matthew J Lopez <lopezm48@msu.edu>
2026-03-01 16:37:47 -0500

    idk

---
+++ insertionsort.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file insertionsort.cpp
+ * @author matth
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+
+    return 0;
+}
---
+++ mergesort.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file mergesort.cpp
+ * @author matth
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+
+    return 0;
+}
