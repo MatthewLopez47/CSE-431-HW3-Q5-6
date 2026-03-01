0318fe57cb26b43971db8721ecaa1b943a88060c
Matthew J Lopez <lopezm48@msu.edu>
2026-03-01 16:56:40 -0500

    abcdefg

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,14 +1,95 @@
-/**
- * @file mergesort.cpp
- * @author matth
- */
-
 #include <iostream>
+#include <vector>
+#include <random>
+#include <string>
+#include <ctime>
+
 using namespace std;
 
-int main()
+// Merges two subarrays of arr[].
+// First subarray is arr[left..mid]
+// Second subarray is arr[mid+1..right]
+void merge(vector<int>& arr, int left, int mid, int right){
+
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    // Create temp vectors
+    vector<int> L(n1), R(n2);
+
+    // Copy data to temp vectors L[] and R[]
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0;
+    int k = left;
+
+    // Merge the temp vectors back
+    // into arr[left..right]
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[],
+    // if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[],
+    // if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+// begin is for left index and end is right index
+// of the sub-array of arr to be sorted
+void mergeSort(vector<int>& arr, int left, int right){
+
+    if (left >= right)
+        return;
+
+    int mid = left + (right - left) / 2;
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    merge(arr, left, mid, right);
+}
+
+
+
+int main(int argc, char const *argv[])
 {
-    cout << "Hello World!\n";
+    // file name, N, seed
+    vector<int> arr(stoi(argv[1]));
+    srand(stoi(argv[2]));
+
+    for (int i = 0; i < arr.size(); i++)
+        arr[i] = rand();
+
+    std::clock_t start_time = std::clock();
+    for (int i = 0; i < 1000; i++)
+    {
+        mergeSort(arr, 0, arr.size() - 1);
+    }
+    std::clock_t tot_time = std::clock() - start_time;
+    std::cout << "Time: "
+              << ((double) tot_time) / (double) CLOCKS_PER_SEC
+              << " seconds" << std::endl;
 
     return 0;
 }
